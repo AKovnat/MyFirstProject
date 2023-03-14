b85885cf2c2b3b846a6b263934667abc0ff371a8
Andrew Kovnat <107608814+AKovnat@users.noreply.github.com>
2023-03-14 15:21:48 +0300

    Update and rename AddExr7.java to AddTask7.java

--- src/main/java/AddTask7.java
+++ src/main/java/AddTask7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddExr7 {
+public class AddTask7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a fractional number: ");

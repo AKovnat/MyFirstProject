e8a3ea2388875a26b8932fed9b209d2eb9bf5fbf
Andrew Kovnat <107608814+AKovnat@users.noreply.github.com>
2023-03-14 15:22:55 +0300

    Update and rename AddExr4.java to AddTask4.java

--- src/main/java/AddTask4.java
+++ src/main/java/AddTask4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddExr4 {
+public class AddTask4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter x: ");

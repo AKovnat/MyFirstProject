a11a789a1f36cbbf78b1925f53e188a5adab389d
Andrew Kovnat <107608814+AKovnat@users.noreply.github.com>
2023-03-14 15:22:13 +0300

    Update and rename AddExr6.java to AddTask6.java

--- src/main/java/AddTask6.java
+++ src/main/java/AddTask6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AddExr6 {
+public class AddTask6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number: ");
